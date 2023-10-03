1bfaaadd0215038f8cb164f94c654a86bcf5b552
ringotc <ringotc@outlook.com>
2023-10-03 11:05:36 -0700

    a3

---
+++ src/Labs/a3/WorkingWithArrays.js
@@ -0,0 +1,42 @@
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingDataToFromArrays from "./AddingAndRemovingDataToFromArrays";
+import MapFunction from "./MapFunction";
+import JsonStringify from "./JsonStringify";
+import FindFunction from "./FindFunction";
+import FilterFunction from "./FilterFunction";
+
+function WorkingWithArrays() {
+  let functionScoped = 2;
+  let blockScoped = 5;
+  const constant1 = functionScoped - blockScoped;
+  let numberArray1 = [1, 2, 3, 4, 5];
+  let stringArray1 = ["string1", "string2"];
+
+  let variableArray1 = [
+    functionScoped,
+    blockScoped,
+    constant1,
+    numberArray1,
+    stringArray1,
+  ];
+
+  return (
+    <div>
+      <h1>Working with Arrays</h1>
+      numberArray1: {numberArray1}
+      <br />
+      stringArray1: {stringArray1}
+      <br />
+      variableArray1: {variableArray1}
+      <br />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingDataToFromArrays />
+      <MapFunction />
+      <JsonStringify />
+      <FindFunction />
+      <FilterFunction />
+    </div>
+  );
+}
+
+export default WorkingWithArrays;
