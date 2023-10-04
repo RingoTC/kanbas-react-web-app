d44b1f06d60d9f223e14f3fe5b4f653368808fd0
ringotc <ringotc@outlook.com>
2023-10-03 18:16:05 -0700

    a3

---
+++ src/Labs/a3/ConditionalOutput/ConditionalOutputInline.js
@@ -0,0 +1,5 @@
+const ConditionalOutputInline = () => {
+  const loggedIn = true;
+  return loggedIn ? <h2>Welcome If Else</h2> : <h2>Please login If Else</h2>;
+};
+export default ConditionalOutputInline;
--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,10 +1,19 @@
+import Classes from "./Classes";
+import Styles from "./Styles";
 import JavaScript from "./JavaScript";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoList from "./todo/TodoList";
+
 function Assignment3() {
-    return (
-        <div>
-            <h1>Assignment 3</h1>
-            <JavaScript/>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Assignment 3</h1>
+      <ConditionalOutput />
+      <Styles />
+      <Classes />
+      <JavaScript />
+      <TodoList />
+    </div>
+  );
 }
 export default Assignment3;
