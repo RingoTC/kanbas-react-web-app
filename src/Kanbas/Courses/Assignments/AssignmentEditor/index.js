21348d99c9c2617032961acbef5b8d988b438fd3
ringotc <ringotc@outlook.com>
2023-11-02 19:15:31 -0700

    a4

--- src/Kanbas/Courses/Assignments/AssignmentEditor/index.js
+++ src/Kanbas/Courses/Assignments/AssignmentEditor/index.js
@@ -1,5 +1,4 @@
 import { Button } from "react-bootstrap";
-import styles from "../../../../index.css";
 import { FaCircleCheck, FaEllipsisVertical } from "react-icons/fa6";
 import { useNavigate, useParams } from "react-router";
 import { Link } from "react-router-dom";
@@ -16,14 +15,14 @@ const CourseAssignmentEditor = () => {
   const assignments = useSelector(
     (state) => state.assignmentsReducer.assignments,
   );
-  const assignment = useSelector((state) => {
-    return state.assignmentsReducer.assignment;
-  });
+  const assignment = useSelector(
+    (state) => state.assignmentsReducer.assignment,
+  );
 
   const dispatch = useDispatch();
   const { assignmentId, courseId } = useParams();
-
   const navigate = useNavigate();
+
   const handleSave = () => {
     dispatch(updateAssignment(assignment));
     navigate(`/Kanbas/Courses/${courseId}/Assignments`);
@@ -57,127 +56,109 @@ const CourseAssignmentEditor = () => {
   }, [dispatch, assignments]);
 
   return (
-    <div className="flex-grow-1" style={{ margin: "20px 30px" }}>
-      <div>
-        <div className="d-flex">
-          <div
-            className="flex-grow-1"
-            style={{ marginLeft: "30px", marginRight: "30px" }}
-          >
-            <div className="wd-flex-row-container">
-              <div className="wd-flex-grow-1"></div>
-              <div className="d-flex float-end main-content-control">
-                <div className="flex-grow-1"></div>
-                <p
-                  style={{
-                    marginTop: "7px",
-                    marginRight: "10px",
-                    color: "green",
-                  }}
-                >
-                  <FaCircleCheck style={{ marginRight: "3px" }} />
-                  Published
-                </p>
-                <Button style={{ background: "#eeeeee", height: "38px" }}>
-                  <FaEllipsisVertical style={{ color: "black" }} />
-                </Button>
-              </div>
+    <div className="container mt-4">
+      <div className="d-flex">
+        <div className="flex-grow-1">
+          <div className="d-flex justify-content-between mb-3">
+            <div className="flex-grow-1"></div>
+            <div className="d-flex float-end main-content-control">
+              <div className="flex-grow-1"></div>
+              <p>
+                <FaCircleCheck /> Published
+              </p>
+              <Button>
+                <FaEllipsisVertical />
+              </Button>
             </div>
-            <div className="mb-3">
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              defaultValue={assignment.title}
+              placeholder="Enter title"
+              onChange={(e) =>
+                dispatch(
+                  setAssignment({ ...assignment, title: e.target.value }),
+                )
+              }
+            />
+          </div>
+          <div className="mb-3">
+            <textarea
+              defaultValue={assignment.description}
+              placeholder="Enter the Assignment Description"
+              className="form-control"
+              onChange={(e) =>
+                dispatch(
+                  setAssignment({
+                    ...assignment,
+                    description: e.target.value,
+                  }),
+                )
+              }
+            />
+          </div>
+          <div className="row">
+            <div className="col-2">Points</div>
+            <div className="col-6">
               <input
                 type="text"
                 className="form-control"
-                defaultValue={assignment.title}
-                placeholder="Enter title"
-                onChange={(e) =>
-                  dispatch(
-                    setAssignment({ ...assignment, title: e.target.value }),
-                  )
-                }
-              />
-            </div>
-            <div className="mb-3">
-              <textarea
-                defaultValue={assignment.description}
-                placeholder="Enter the Assignment Description"
-                className="form-control"
+                defaultValue={assignment.points}
+                placeholder="Enter the points"
                 onChange={(e) =>
                   dispatch(
                     setAssignment({
                       ...assignment,
-                      description: e.target.value,
+                      points: e.target.value,
                     }),
                   )
                 }
               />
             </div>
-            <div className="container">
-              <div className="row">
-                <div className="col-2">Points</div>
+          </div>
+          <div className="row mt-3">
+            <div className="col-2">Due</div>
+            <div className="col-6">
+              <input
+                type="date"
+                className="form-control"
+                defaultValue={assignment.dueDate}
+              />
+              <div className="row mt-2">
                 <div className="col-6">
-                  <input
-                    type="text"
-                    className="form-control"
-                    defaultValue={assignment.points}
-                    placeholder="Enter the points"
-                    onChange={(e) =>
-                      dispatch(
-                        setAssignment({
-                          ...assignment,
-                          points: e.target.value,
-                        }),
-                      )
-                    }
-                  />
-                </div>
-              </div>
-              <br />
-
-              <div className="row">
-                <div className="col-2">Due</div>
-                <div className="col-6">
+                  Available From
                   <input
                     type="date"
                     className="form-control"
-                    defaultValue={assignment.dueDate}
+                    defaultValue={assignment.availableFrom}
                   />
-                  <br />
-                  <div className="row">
-                    <div className="col-3">
-                      Available From
-                      <input
-                        type="date"
-                        className="form-control"
-                        defaultValue={assignment.availableFrom}
-                      />
-                    </div>
-                    <div className="col-3">
-                      Available Until
-                      <input
-                        type="date"
-                        className="form-control"
-                        defaultValue={assignment.availableUntil}
-                      />
-                    </div>
-                  </div>
+                </div>
+                <div className="col-6">
+                  Available Until
+                  <input
+                    type="date"
+                    className="form-control"
+                    defaultValue={assignment.availableUntil}
+                  />
                 </div>
               </div>
-              <br />
-              <hr />
-              <div className="col float-end">
-                <button className="btn">
-                  <Link
-                    to={`/Kanbas/Courses/${courseId}/Assignments`}
-                    className="btn btn-secondary"
-                  >
-                    Cancel
-                  </Link>
-                </button>
-                <button className="btn btn-danger" onClick={handleSave}>
-                  Save
-                </button>
-              </div>
             </div>
+          </div>
+          <hr />
+          <div className="d-flex justify-content-end">
+            <button className="btn btn-secondary me-2">
+              <Link
+                to={`/Kanbas/Courses/${courseId}/Assignments`}
+                className="text-white text-decoration-none"
+              >
+                Cancel
+              </Link>
+            </button>
+            <button className="btn btn-danger" onClick={handleSave}>
+              Save
+            </button>
           </div>
         </div>
       </div>
--- src/Kanbas/Courses/Assignments/index.js
+++ src/Kanbas/Courses/Assignments/index.js
@@ -18,7 +18,7 @@ function Assignments() {
 
   return (
     <div>
-      <div className="flex-grow-1" style={{ margin: "30px" }}>
+      <div className="flex-grow-1">
         <div className="d-flex flex-row">
           <div className="search flex-grow-1">
             <input
@@ -29,48 +29,34 @@ function Assignments() {
           </div>
           <div className="d-flex float-end main-content-control">
             <div className="flex-grow-1"></div>
-            <button className="btn" style={{ background: "#eeeeee" }}>
-              <FaPlus style={{ marginRight: "3px" }} />
+            <button className="btn">
+              <FaPlus />
               Group
             </button>
             <button className="btn btn-danger">
-              <FaPlus style={{ marginRight: "3px" }} />
+              <FaPlus />
               Assignment
             </button>
-            <button
-              className="btn"
-              style={{ background: "#eeeeee", height: "38px" }}
-            >
+            <button className="btn">
               <FaEllipsisVertical />
             </button>
           </div>
         </div>
 
         <hr />
-        <ul className="wd-flex-grow-1 list-group module-list">
+        <ul className="list-group">
           <li className="list-group-item list-group-item-secondary">
-            <FontAwesomeIcon
-              icon={faGripVertical}
-              style={{ marginRight: "5px" }}
-            />
+            <FontAwesomeIcon icon={faGripVertical} />
             <span>
               <strong>Assignments</strong>
             </span>
             <div className="float-end">
-              <button
-                className="btn rounded-pill"
-                style={{ background: "#eeeeee", marginRight: "20px" }}
-              >
-                40% of Total
-              </button>
-              <FaPlus style={{ marginRight: "20px" }} />
+              <button className="btn rounded-pill">40% of Total</button>
+              <FaPlus />
               <FaEllipsisVertical />
             </div>
           </li>
-          <ul
-            className="list-group"
-            style={{ borderRadius: "0", borderLeft: "5px solid green" }}
-          >
+          <ul className="list-group">
             {assignments
               .filter((module) => module.course === courseId)
               .map((assignment) => (
@@ -86,12 +72,9 @@ function Assignments() {
                         Delete
                       </button>
                     </div>
-                    <FontAwesomeIcon
-                      icon={faGripVertical}
-                      style={{ marginRight: "20px" }}
-                    />
-                    <FaBook style={{ color: "green", marginRight: "10px" }} />
-                    <span style={{ display: "inline" }}>
+                    <FontAwesomeIcon icon={faGripVertical} />
+                    <FaBook />
+                    <span>
                       <strong>
                         <Link
                           to={`/Kanbas/Courses/${courseId}/Assignments/${assignment.id}`}
@@ -101,25 +84,14 @@ function Assignments() {
                         </Link>
                       </strong>
                     </span>
-                    <div
-                      style={{
-                        marginLeft: "70px",
-                        color: "#686464",
-                        width: "600px",
-                      }}
-                    >
-                      <span style={{ fontSize: "15px", marginBottom: "1px" }}>
-                        {assignment?.description}
-                      </span>
-                      <p>{assignment.due}</p>
+                    <div>
+                      <span>{assignment?.description}</span>
                     </div>
                   </div>
                 </li>
               ))}
           </ul>
         </ul>
-        <br />
-        <br />
       </div>
     </div>
   );
