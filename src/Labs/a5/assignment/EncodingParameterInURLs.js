4b4f8582c8bffb93da5d42e2fcb3decb16b5ed11
ringotc <ringotc@outlook.com>
2023-11-20 16:42:24 -0800

    a5

--- src/Labs/a5/assignment/EncodingParameterInURLs.js
+++ src/Labs/a5/assignment/EncodingParameterInURLs.js
@@ -1,27 +1,11 @@
 import React, { useState } from "react";
-
 function EncodingParametersInURLs() {
   const [a, setA] = useState(34);
   const [b, setB] = useState(23);
-
-  const baseUrl =
-    "https://kanbas-node-server-app-cs5610-fa23-3jx3.onrender.com/a5";
-  const calculatorPath = "calculator";
-
-  const createOperationLink = (operation) => {
-    return `${baseUrl}/${calculatorPath}?operation=${operation}&a=${a}&b=${b}`;
-  };
-
-  const createPathParameterLink = (operation) => {
-    return `${baseUrl}/${operation}/${a}/${b}`;
-  };
-
   return (
     <div>
       <h3>Encoding Parameters In URLs</h3>
       <h4>Calculator</h4>
-
-      {/* Input fields for values of 'a' and 'b' */}
       <input
         onChange={(e) => setA(e.target.value)}
         className="form-control"
@@ -34,29 +18,35 @@ function EncodingParametersInURLs() {
         type="number"
         value={b}
       />
-
       <h3>Path Parameters</h3>
-
-      {/* Buttons for path parameter operations */}
-      <a href={createPathParameterLink("add")} className="btn btn-primary">
+      <a
+        href={`https://kanbas-node-server-app-cs5610-fa23-3jx3.onrender.com/a5/add/${a}/${b}`}
+        className="btn btn-primary"
+      >
         Add {a} + {b}
       </a>
-      <a href={createPathParameterLink("subtract")} className="btn btn-danger">
-        Subtract {a} - {b}
+      <a
+        href={`https://kanbas-node-server-app-cs5610-fa23-3jx3.onrender.com/a5/subtract/${a}/${b}`}
+        className="btn btn-danger"
+      >
+        Substract {a} - {b}
       </a>
 
       <hr />
       <h3>Query Parameters</h3>
-
-      {/* Buttons for query parameter operations */}
-      <a href={createOperationLink("add")} className="btn btn-primary">
+      <a
+        href={`https://kanbas-node-server-app-cs5610-fa23-3jx3.onrender.com/a5/calculator?operation=add&a=${a}&b=${b}`}
+        className="btn btn-primary"
+      >
         Add {a} + {b}
       </a>
-      <a href={createOperationLink("subtract")} className="btn btn-danger">
-        Subtract {a} - {b}
+      <a
+        href={`https://kanbas-node-server-app-cs5610-fa23-3jx3.onrender.com/a5/calculator?operation=subtract&a=${a}&b=${b}`}
+        className="btn btn-danger"
+      >
+        Substract {a} - {b}
       </a>
     </div>
   );
 }
-
 export default EncodingParametersInURLs;
--- src/Labs/a5/assignment/WorkingWithObjects.js
+++ src/Labs/a5/assignment/WorkingWithObjects.js
@@ -2,55 +2,24 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function WorkingWithObjects() {
-  const initialAssignment = {
+  const [assignment, setAssignment] = useState({
     id: 1,
     title: "NodeJS Assignment",
     description: "Create a NodeJS server with ExpressJS",
     due: "2021-10-10",
     completed: false,
     score: 0,
-  };
-
-  const [assignment, setAssignment] = useState(initialAssignment);
+  });
   const URL =
     "https://kanbas-node-server-app-cs5610-fa23-3jx3.onrender.com/a5/assignment";
-
   const fetchAssignment = async () => {
-    try {
-      const response = await axios.get(URL);
-      setAssignment(response.data);
-    } catch (error) {
-      console.error("Error fetching assignment:", error);
-    }
+    const response = await axios.get(`${URL}`);
+    setAssignment(response.data);
   };
-
   const updateTitle = async () => {
-    try {
-      const response = await axios.get(`${URL}/title/${assignment.title}`);
-      setAssignment(response.data);
-    } catch (error) {
-      console.error("Error updating title:", error);
-    }
+    const response = await axios.get(`${URL}/title/${assignment.title}`);
+    setAssignment(response.data);
   };
-
-  const updateScore = async () => {
-    try {
-      await axios.post(`${URL}/score`, { score: assignment.score });
-      fetchAssignment();
-    } catch (error) {
-      console.error("Error updating score:", error);
-    }
-  };
-
-  const updateCompleted = async () => {
-    try {
-      await axios.post(`${URL}/completed`, { completed: assignment.completed });
-      fetchAssignment();
-    } catch (error) {
-      console.error("Error updating completed status:", error);
-    }
-  };
-
   useEffect(() => {
     fetchAssignment();
   }, []);
@@ -58,56 +27,79 @@ function WorkingWithObjects() {
   return (
     <div>
       <h3>Working With Objects</h3>
-
       <h4>Modifying Properties</h4>
-      <button onClick={updateTitle} className="w-100 btn btn-primary mb-2">
-        Update Title to: {assignment.title}
-      </button>
+      <a
+        href={`${URL}/title/${assignment.title}`}
+        className="btn btn-primary me-2 float-end"
+      >
+        Update Title
+      </a>
       <input
         onChange={(e) =>
-          setAssignment({ ...assignment, title: e.target.value })
+          setAssignment({
+            ...assignment,
+            title: e.target.value,
+          })
         }
         value={assignment.title}
         className="form-control mb-2 w-75"
         type="text"
       />
 
+      <button onClick={updateTitle} className="w-100 btn btn-primary mb-2">
+        Update Title to: {assignment.title}
+      </button>
       <button onClick={fetchAssignment} className="w-100 btn btn-danger mb-2">
         Fetch Assignment
       </button>
 
       <h4>Retrieving Objects</h4>
-      <a href={URL} className="btn btn-primary me-2">
+      <a
+        href="https://kanbas-node-server-app-cs5610-fa23-3jx3.onrender.com/a5/assignment"
+        className="btn btn-primary me-2"
+      >
         Get Assignment
       </a>
-
       <h4>Retrieving Properties</h4>
-      <a href={`${URL}/title`} className="btn btn-primary me-2">
+      <a
+        href="https://kanbas-node-server-app-cs5610-fa23-3jx3.onrender.com/a5/assignment/title"
+        className="btn btn-primary me-2"
+      >
         Get Title
       </a>
-
       <h4>Updating Score</h4>
-      <button onClick={updateScore} className="w-100 btn btn-primary mb-2">
+      <a
+        href={`${URL}/score/${assignment.score}`}
+        className="btn btn-primary me-2 float-end"
+      >
         Update Score
-      </button>
+      </a>
       <input
         onChange={(e) =>
-          setAssignment({ ...assignment, score: e.target.value })
+          setAssignment({
+            ...assignment,
+            score: e.target.value,
+          })
         }
         value={assignment.score}
         className="form-control mb-2 w-75"
         type="number"
       />
-
       <h4>Updating Completed</h4>
-      <button onClick={updateCompleted} className="w-100 btn btn-primary mb-2">
+      <a
+        href={`${URL}/completed/${assignment.completed}`}
+        className="btn btn-primary me-2 float-end"
+      >
         Update Completed
-      </button>
+      </a>
 
       <div className="form-check">
         <input
           onChange={(e) =>
-            setAssignment({ ...assignment, completed: e.target.checked })
+            setAssignment({
+              ...assignment,
+              completed: e.target.checked,
+            })
           }
           checked={assignment.completed}
           className="form-check-input"
@@ -121,5 +113,4 @@ function WorkingWithObjects() {
     </div>
   );
 }
-
 export default WorkingWithObjects;
