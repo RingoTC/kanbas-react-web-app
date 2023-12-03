fc151f5755a22df5d0b92599e7fd07a78cabca19
ringotc <ringotc@outlook.com>
2023-12-02 22:55:36 -0800

    a6 env

--- src/Kanbas/Courses/Assignments/client.js
+++ src/Kanbas/Courses/Assignments/client.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE = process.env.API_BASE || "http://localhost:4000";
+const API_BASE = process.env.REACT_APP_BASE_URL || "http://localhost:4000";
 const COURSES_URL = `${API_BASE}/api/courses`;
 const ASSIGNMENTS_URL = `${API_BASE}/api/assignments`;
 export const findAssignmentsForCourse = async (courseId) => {
